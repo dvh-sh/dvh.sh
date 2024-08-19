e3725d6222f96cf586dff55de998c3aa51df7d82
dvhsh <david@dvh.sh>
2024-08-19 11:15:27 -0700

    massive rewrite

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { NextUIProvider } from "@nextui-org/react";
 import type { Metadata }  from 'next'
 import      { Inter    }  from 'next/font/google'
 
-import Header             from "@container/header/Header";
 import Footer             from "@container/footer/Footer";
 
 import './globals.css'
@@ -11,27 +10,48 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title       : 'dvh.sh',
-  description : 'personal developer portfolio',
+    title       : 'David | Full-Stack Developer Portfolio',
+    description : 'Explore David’s personal portfolio showcasing skills and experience in full-stack development, with expertise in various modern technologies.',
+    keywords    : 'David, Full-Stack Developer, Software Engineer, Web Development, Backend, Frontend, CSIS Student',
+    openGraph   : {
+        title       : 'David | Full-Stack Developer Portfolio',
+        description : 'Explore David’s portfolio, highlighting his expertise in full-stack development across a range of modern technologies.',
+        url         : 'https://dvh.sh',
+        siteName    : 'David Portfolio',
+        locale      : 'en_US',
+        type        : 'website',
+    }
 }
-
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-        <body className={inter.className}>
+        <head>
+            <meta charSet="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta name="robots" content="index, follow" />
+            <link rel="canonical" href="https://dvh.sh" />
+            <meta name="description" content="Explore David’s personal portfolio showcasing skills and experience in full-stack development, with expertise in various modern technologies." />
+            <meta name="keywords" content="David, Full-Stack Developer, Software Engineer, Web Development, Backend, Frontend, CSIS Student" />
+            <meta name="author" content="David" />
+            <meta property="og:title" content="David | Full-Stack Developer Portfolio" />
+            <meta property="og:description" content="Explore David’s portfolio, highlighting his expertise in full-stack development across a range of modern technologies." />
+            <meta property="og:url" content="https://dvh.sh" />
+            <meta property="og:site_name" content="David Portfolio" />
+            <meta property="og:type" content="website" />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:site" content="@HaruhimeHoldings" />
+            <meta name="twitter:title" content="David | Full-Stack Developer Portfolio" />
+            <meta name="twitter:description" content="Explore David’s portfolio, highlighting his expertise in full-stack development across a range of modern technologies." />
+        </head>
+        <body className={`${inter.className}`}>
         <NextUIProvider>
-            <div className="flex flex-col min-h-screen">
-                <Header />
-                <div className="flex-grow">
-                    {children}
-                </div>
-                <Footer />
-            </div>
+            <main className="flex-grow">{children}</main>
+            <Footer />
         </NextUIProvider>
         </body>
         </html>
-    )
+    );
 }
 
 // path: src/app/layout.tsx
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,71 @@
-import Landing from "@container/landing/Landing";
+import {
+    SiJetbrains,
+    SiTypescript,
+    SiPython,
+    SiKotlin,
+    SiCplusplus,
+    SiGit,
+    SiGithub,
+    SiLinkedin,
+    SiGmail
+}                                               from "react-icons/si";
+
+import { Card, CardBody, Image, Link, Divider } from "@nextui-org/react";
+
+import TextTyper                                from "@component/text/TextTyper";
 
 export default function Page() {
     return (
-        <div className="flex flex-grow justify-center items-center p-4 sm:p-6">
-            <Landing />
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-gray-800 to-gray-900 p-6">
+            <Card className="bg-gray-800 rounded-3xl shadow-2xl max-w-2xl p-8">
+                <CardBody className="flex flex-col items-center">
+                    <Image
+                        src="https://avatars.githubusercontent.com/u/175678329?v=4"
+                        alt="avatar"
+                        width={120}
+                        height={120}
+                        className="rounded-full border-4 border-gray-600 shadow-lg mb-6"
+                    />
+                    <h1 className="text-center text-white text-xl font-bold mb-4"><TextTyper/></h1>
+                    <p className="text-gray-300 text-center mb-6">
+                        CSIS Student, Software Developer, and Entrepreneur. <br/>
+                        Founder & CTO @ <Link isExternal href="https://haruhime.holdings" className="hover:underline text-blue-400">Haruhime Holdings</Link>
+                    </p>
+
+                    <Divider className="my-6 bg-gray-500"/>
+
+                    <h2 className="text-white text-xl font-semibold mb-4">Skills</h2>
+                    <div className="flex justify-center space-x-6 mb-6">
+                        <SiTypescript className="text-blue-400 w-10 h-10 hover:scale-110 transition-transform duration-300" />
+                        <SiPython className="text-yellow-400 w-10 h-10 hover:scale-110 transition-transform duration-300" />
+                        <SiKotlin className="text-purple-400 w-10 h-10 hover:scale-110 transition-transform duration-300" />
+                        <SiCplusplus className="text-blue-400 w-10 h-10 hover:scale-110 transition-transform duration-300" />
+                    </div>
+
+                    <h3 className="text-white text-lg font-semibold mb-2">Tools</h3>
+                    <div className="flex justify-center space-x-6 mb-6">
+                        <SiGit className="text-orange-400 w-10 h-10 hover:scale-110 transition-transform duration-300" />
+                        <SiJetbrains className="text-purple-400 w-10 h-10 hover:scale-110 transition-transform duration-300" />
+                    </div>
+
+                    <Divider className="my-6 bg-gray-500"/>
+
+                    <h2 className="text-white text-xl font-semibold mb-4">Connect with Me</h2>
+                    <div className="flex justify-center space-x-6">
+                        <a href={"https://github.com/dvhsh"} target={"_blank"} className="fill-slate-400" aria-label="Github Redirect" >
+                            <SiGithub className="text-gray-400 w-10 h-10 hover:text-white hover:scale-110 transition-transform duration-300" />
+                        </a>
+
+                        <a href={"https://www.linkedin.com/in/dvhsh"} target={"_blank"} className="fill-slate-400" aria-label="LinkedIn Redirect" >
+                            <SiLinkedin className="text-gray-400 w-10 h-10 hover:text-white hover:scale-110 transition-transform duration-300" />
+                        </a>
+
+                        <a href={"mailto:david@dvh.sh"} target={"_blank"} className="fill-slate-400" aria-label="Email Redirect" >
+                            <SiGmail className="text-gray-400 w-10 h-10 hover:text-white hover:scale-110 transition-transform duration-300" />
+                        </a>
+                    </div>
+                </CardBody>
+            </Card>
         </div>
     );
 }
