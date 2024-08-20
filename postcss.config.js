401365231299b0b4560b265b5ef1799d63712fac
dvhsh <david@dvh.sh>
2024-08-19 21:24:53 -0700

    update postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,13 @@
-const tailwindcss = require('tailwindcss');
-const autoprefixer = require('autoprefixer');
-const purgecss = require('@fullhuman/postcss-purgecss');
-
 module.exports = {
     plugins: [
-        tailwindcss,
-        autoprefixer,
-        process.env.NODE_ENV === 'production'
-            ? purgecss({
+        'tailwindcss',
+        'autoprefixer',
+        process.env.NODE_ENV === 'production' ? [
+            '@fullhuman/postcss-purgecss', {
                 content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}', './app/**/*.{js,jsx,ts,tsx}'],
                 safelist: ['html', 'body'], // Keep these in
                 defaultExtractor: content => content.match(/[\w-/.:]+(?<!:)/g) || [],
-            })
-            : undefined,
+            }
+        ] : undefined,
     ].filter(Boolean),
 };
