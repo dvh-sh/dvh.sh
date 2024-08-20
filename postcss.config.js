47f25c71e258c610bcc7694cdd9f53c8c017d803
dvhsh <david@dvh.sh>
2024-08-19 21:33:37 -0700

    remove posscss csspurge

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,6 @@
 module.exports = {
     plugins: [
         'tailwindcss',
-        'autoprefixer',
-        process.env.NODE_ENV === 'production' ? [
-            '@fullhuman/postcss-purgecss', {
-                content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}', './app/**/*.{js,jsx,ts,tsx}'],
-                safelist: ['html', 'body'], // Keep these in
-                defaultExtractor: content => content.match(/[\w-/.:]+(?<!:)/g) || [],
-            }
-        ] : undefined,
-    ].filter(Boolean),
+        'autoprefixer'
+    ]
 };
