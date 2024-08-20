ff2bfcd66554a31941afddf82aad732fa86b4e6e
dvhsh <david@dvh.sh>
2024-08-19 21:23:13 -0700

    update postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,17 @@
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+const purgecss = require('@fullhuman/postcss-purgecss');
+
 module.exports = {
-  plugins: [
-    'tailwindcss',
-    'autoprefixer',
-    process.env.NODE_ENV === 'production'
-        ? {
-          '@fullhuman/postcss-purgecss': {
-            content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}', './app/**/*.{js,jsx,ts,tsx}'],
-            safelist: ['html', 'body'], // Keep these in
-            defaultExtractor: content => content.match(/[\w-/.:]+(?<!:)/g) || [],
-          },
-        }
-        : undefined,
-  ].filter(Boolean),
+    plugins: [
+        tailwindcss,
+        autoprefixer,
+        process.env.NODE_ENV === 'production'
+            ? purgecss({
+                content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}', './app/**/*.{js,jsx,ts,tsx}'],
+                safelist: ['html', 'body'], // Keep these in
+                defaultExtractor: content => content.match(/[\w-/.:]+(?<!:)/g) || [],
+            })
+            : undefined,
+    ].filter(Boolean),
 };
