da25d5ec0abb09411124b0159e938bc90a0205a3
dvhsh <david@dvh.sh>
2024-09-22 23:42:42 -0700

    update AboutMe wording

--- src/component/content/AboutMe.tsx
+++ src/component/content/AboutMe.tsx
@@ -4,25 +4,27 @@ export default function AboutMe() {
       <h2 className="text-3xl font-bold mb-4 text-accent">Hi, I'm David 👋</h2>
       <div className="space-y-4 text-text">
         <p>
-          I'm a self-taught fullstack software engineer with over 7 years of
-          experience. Currently, I'm balancing my software development work with
-          full-time studies.
+          I’m a self-taught full-stack software engineer with over seven years
+          of experience. Currently, I juggle my software development
+          responsibilities with full-time studies.
         </p>
         <p>
-          Outside of coding, I enjoy hiking, photography, and horticulture. When
-          I need to unwind, you'll often find me playing Tetris - it's my go-to
-          game for relaxation.
+          Outside of coding, I find solace in hiking, photography, and
+          horticulture. When I need to unwind, you’ll often find me engrossed in
+          playing Tetris - it’s my go-to game for relaxation.
         </p>
         <p>
-          In the tech world, I have a particular interest in OSINT and some
-          background in reverse engineering. I've also worked extensively with
-          DevOps, streamlining development processes and improving efficiency.
+          In the tech world, I have a particular interest in Open Source
+          Intelligence (OSINT) and some background in reverse engineering.
+          Additionally, I’ve had extensive experience working with DevOps, where
+          I’ve successfully streamlined development processes and enhanced
+          efficiency.
         </p>
         <p>
           My past projects have involved low-level programming and optimization,
-          particularly in areas that required innovative approaches to
-          problem-solving. These experiences have honed my skills in creating
-          efficient, high-performance solutions.
+          particularly in areas that demanded innovative problem-solving
+          approaches. These experiences have honed my skills in crafting
+          efficient and high-performance solutions.
         </p>
       </div>
     </section>
