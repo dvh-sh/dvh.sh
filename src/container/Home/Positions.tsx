ffb919dc28b0240c3b33ad25bb65880d58e44cc1
dvhsh <david@dvh.sh>
2024-11-12 23:57:42 -0800

    update positions text

--- src/container/Home/Positions.tsx
+++ src/container/Home/Positions.tsx
@@ -9,7 +9,10 @@ interface PositionsProps {
 const Positions = ({ data }: PositionsProps) => {
   return (
     <section id="positions" className="mb-16">
-      <h2 className="text-6xl font-bold mb-4 text-accent">Positions</h2>
+      <h2 className="text-6xl font-bold mb-4 text-accent">
+        Positions
+        <h6 className="text-sm">(Most unlisted)</h6>
+      </h2>
       <div className="space-y-8">
         {data.map((position, index) => (
           <MotionDiv key={position.title} delay={index * 0.2}>
